0cd1611e40eb3327f96339a3ae187754e3618464
Christian J. Barratt <cbarratt.dev@gmail.com>
2020-03-26 17:26:22 -0400

    commit --css updates

---
+++ PaperTrail.Web/wwwroot/theme/dist/js/customscripts.js
@@ -0,0 +1,47 @@
+// const navbarActive = document.querySelector('.nav-sidebar');
+// li = navbarActive.querySelectorAll('li');
+
+// li.forEach(function(e) {
+//     e.addEventListener('click', function() {
+//         for (let i = 0; i < li.length; i++) {
+//             li[i].classList.remove('active');
+//         }
+//         this.classList.add('active');
+//     });
+// });
+
+
+$(document).ready(function() {
+    $.each($('.nav-sidebar').find('li'), function(){
+        $(this).toggleClass('active', window.location.pathname.indexOf($(this).find('a').attr('asp-controller')) > -1)
+    });
+});
+
+
+
+$(function () {
+    $('#catalogId').DataTable({
+      "paging": true,
+      "lengthChange": false,
+      "searching": false,
+      "ordering": false,
+      "info": true,
+      "autoWidth": false,
+    });
+    $('#checkoutList').DataTable({
+      "paging": true,
+      "lengthChange": false,
+      "searching": false,
+      "ordering": false,
+      "info": true,
+      "autoWidth": false,
+    });
+    $('#reserveList').DataTable({
+      "paging": true,
+      "lengthChange": false,
+      "searching": false,
+      "ordering": true,
+      "info": true,
+      "autoWidth": false,
+    });
+  });
